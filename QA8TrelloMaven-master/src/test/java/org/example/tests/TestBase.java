c9fbf5cf6de779e6851245d767ffcf76ac0f3eff
AntonMinkou <5085047a@gmail.com>
2021-04-30 11:28:52 +0300

    Initial commit

---
+++ QA8TrelloMaven-master/src/test/java/org/example/tests/TestBase.java
@@ -0,0 +1,78 @@
+package org.example.tests;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.example.SuiteConfiguration;
+import org.example.pages.HomePageHelper;
+import org.example.util.LogLog4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Capabilities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import ru.stqa.selenium.factory.WebDriverPool;
+
+/**
+ * Base class for TestNG-based test classes
+ */
+public class TestBase {
+
+    protected static URL gridHubUrl = null;
+    protected static String baseUrl;
+    protected static Capabilities capabilities;
+    public static LogLog4j log4j = new LogLog4j();
+
+    //protected WebDriver driver;
+    protected EventFiringWebDriver driver;
+    public HomePageHelper homePage;
+    public static final String LOGIN = "marinaqatest2019@gmail.com";
+    public static final String PASSWORD = "marinaqa";
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println("Find element :" + by);
+
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + "element was not found");
+        }
+    }
+
+
+    @BeforeSuite
+    public void initTestSuite() throws IOException {
+        SuiteConfiguration config = new SuiteConfiguration();
+        baseUrl = config.getProperty("site.url");
+        if (config.hasProperty("grid.url") && !"".equals(config.getProperty("grid.url"))) {
+            gridHubUrl = new URL(config.getProperty("grid.url"));
+        }
+        capabilities = config.getCapabilities();
+    }
+
+    @BeforeMethod
+    public void initWebDriver() {
+
+        driver = new EventFiringWebDriver(WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities));
+        driver.register(new MyListener());
+        homePage = PageFactory.initElements(driver, HomePageHelper.class);
+        driver.get(baseUrl);
+        homePage.waitUntilPageIsLoaded();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        WebDriverPool.DEFAULT.dismissAll();
+    }
+}
